6be5d894f8ee6ea43594f3b4200c73c0e8aea400
Mayara Delonzek <mayaradelonzek2@gmail.com>
2021-11-08 11:15:59 -0300

    Adicionar teste de validacao do tamanho dos campos para cliente.

--- test/ClienteTest.java
+++ test/ClienteTest.java
@@ -6,7 +6,7 @@ public class ClienteTest {
     @Test
     public void deveCriarClienteComSucesso() {
         Contato contato = new Contato("testerson@teste.com", "555555555");
-        Endereco endereco = new Endereco("Rua teste", "666", "Testeira", "Testing", "90600000", "Testando", "TS");
+        Endereco endereco = new Endereco("Rua teste", "444", "Testeira", "Testing", "90600000", "Testando", "TS");
         Cliente testerson = new Cliente(contato, endereco, "Testerson", "12345678910");
 
         Assert.assertNotNull(contato);
@@ -23,6 +23,21 @@ public class ClienteTest {
         } catch (Exception e) {
             String mensagem = "[O nome deve ser informado., O cpf deve ser informado., O contato deve ser informado., O endereco deve ser informado.]";
             Assert.assertEquals(mensagem, e.getMessage());
+        }
+    }
+
+    @Test
+    public void deveValidarClienteComTamanhoDeCamposInvalidos() {
+        try {
+            Helper helper = new Helper();
+            String tamanhoInvalido = helper.gerarString(101);
+            Contato contato = new Contato("testerson@teste.com", "555555555");
+            Endereco endereco = new Endereco("Rua teste", "444", "Testeira", "Testing", "90600000", "Testando", "TS");
+            Cliente testerson = new Cliente(contato, endereco, tamanhoInvalido, tamanhoInvalido);
+
+        } catch (Exception e) {
+            String mensagemTamanhoInvalido = "[Tamanho do campo nome invalido., Tamanho do campo cpf invalido.]";
+            Assert.assertEquals(mensagemTamanhoInvalido, e.getMessage());
         }
     }
 }
